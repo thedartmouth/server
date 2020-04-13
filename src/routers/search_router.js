2852b1967d6c9a097f3d4c2989d8b119655528f5
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2020-04-13 13:25:26 -0400

    Added demo search response to search router

--- src/routers/search_router.js
+++ src/routers/search_router.js
@@ -1,11 +1,21 @@
 import express from 'express';
 
+// TODO: Remove axios
+import axios from 'axios';
+
 const router = express();
 
 router.route('/')
   .get((req, res) => {
-    console.log('params:', req.query);
-    res.json(req.query);
+    // Test link: http://localhost:9090/search?query=22&page=1
+    console.log('Params:', req.query);
+
+    // Get test JSON from jsonplaceholder and send to frontend for display
+    axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => {
+      res.send(response.data);
+    }).catch((error) => {
+      res.json(error);
+    });
   });
 
 export default router;
