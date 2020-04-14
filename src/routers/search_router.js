8a042c72f10735339a4b2e7839d7a170bf375c9c
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2020-04-14 10:04:20 -0400

    Added working alphabetical sort by title

--- src/routers/search_router.js
+++ src/routers/search_router.js
@@ -9,10 +9,25 @@ router.route('/')
   .get((req, res) => {
     // Test link: http://localhost:9090/search?query=22&page=1
     console.log('Params:', req.query);
+    const {
+      query, filters, sort, page, numPerPage,
+    } = req.query;
 
     // Get test JSON from jsonplaceholder and send to frontend for display
     axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => {
-      res.send(response.data);
+      const tempArray = (response.data || [])
+        // .filter()
+        .sort((a, b) => {
+          if (a.title < b.title) return (sort === 'a' ? -1 : 1);
+          if (a.title > b.title) return (sort === 'a' ? 1 : -1);
+          return 0;
+        });
+        // .slice(10); // numPerPage * page, (numPerPage + 1) * page
+
+      // console.log(tempArray);
+      // console.log(tempArray.slice(0, 10));
+
+      res.send(tempArray);
     }).catch((error) => {
       res.json(error);
     });
