7c57ea9834775f10c62e1e9d69a6a4435042f31c
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2020-04-21 11:34:40 -0400

    Working URL filtering

--- src/controllers/search_controller.js
+++ src/controllers/search_controller.js
@@ -1,14 +1,9 @@
 import Resource from '../models/resource_model';
 
 // eslint-disable-next-line import/prefer-default-export
-export function search(query, filters, sort, page, numPerPage) {
+export function search(queryObject, sort, page, numPerPage) {
   return new Promise((resolve, reject) => {
-    Resource.find(query ? {
-      $or: [
-        { title: { $regex: new RegExp(query, 'i') } }, // Note: regexp can become inefficient for large collections
-        { description: { $regex: new RegExp(query, 'i') } }, // See here: https://stackoverflow.com/questions/7101703/how-do-i-make-case-insensitive-queries-on-mongodb
-      ],
-    } : {}) // Return all results (within limits) if no query
+    Resource.find(queryObject) // Return all results (within limits) if no query
       .sort({ title: sort === 'a' ? -1 : 1 }) // Sort by title
       .skip((page - 1) * numPerPage) // Start at the beginning of the "page"
       .limit(numPerPage) // Limit to the end of the "page"
--- src/routers/search_router.js
+++ src/routers/search_router.js
@@ -8,15 +8,33 @@ router.route('/')
   .get((req, res) => {
     console.log('Params:', req.query);
 
-    const {
-      query, filters, sort,
-    } = req.query;
+    // Add any additional filters here. NOTE: Shorten these for cleanliness in URL
+    const { query, maxValue } = req.query; // Params that don't need defaults
+    const sort = req.query.sort || 'a'; // Params that need defaults
 
     // Parse strings to numbers
-    const page = parseInt(req.query.page, 10);
-    const numPerPage = parseInt(req.query.numperpage, 10);
+    const page = parseInt(req.query.page, 10) || 1;
+    const numPerPage = parseInt(req.query.numperpage, 10) || 10;
+    const maxValNum = parseInt(maxValue, 10);
+    console.log(page, numPerPage, maxValNum);
 
-    search(query, filters, sort, page, numPerPage).then((results) => {
+    const queryObject = {
+      $and: [
+        query ? {
+          $or: [
+            { title: { $regex: new RegExp(query, 'i') } }, // Note: regexp can become inefficient for large collections
+            { description: { $regex: new RegExp(query, 'i') } }, // See here: https://stackoverflow.com/questions/7101703/how-do-i-make-case-insensitive-queries-on-mongodb
+          ],
+        } : {},
+        {
+          $and: [ // Implement filters here
+            { value: { $lte: maxValNum } },
+          ],
+        },
+      ],
+    };
+
+    search(queryObject, sort, page, numPerPage).then((results) => {
       res.send({ results, numResults: results.length });
     }).catch((error) => {
       res.json(error);
