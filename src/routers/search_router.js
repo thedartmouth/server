139b203e4d21fe9ee95911ca038344b0ddb09da2
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2020-04-25 12:03:14 -0400

    Fixed bug with maxValue on search_router

--- src/routers/search_router.js
+++ src/routers/search_router.js
@@ -5,6 +5,8 @@ import { search } from '../controllers/search_controller';
 const router = express();
 
 router.route('/')
+
+  // Main query-based search
   .get((req, res) => {
     console.log('Params:', req.query);
 
@@ -15,25 +17,27 @@ router.route('/')
     // Parse strings to numbers
     const page = parseInt(req.query.page, 10) || 1;
     const numPerPage = parseInt(req.query.numperpage, 10) || 10;
-    const maxValNum = parseInt(maxValue, 10);
-    console.log(page, numPerPage, maxValNum);
 
     const queryObject = {
       $and: [
+        // Query title and description if query sting exists
         query ? {
           $or: [
-            { title: { $regex: new RegExp(query, 'i') } }, // Note: regexp can become inefficient for large collections
+            { title: { $regex: new RegExp(query, 'i') } }, // Note: regexp can become inefficient for large collections, look into indexing
             { description: { $regex: new RegExp(query, 'i') } }, // See here: https://stackoverflow.com/questions/7101703/how-do-i-make-case-insensitive-queries-on-mongodb
           ],
         } : {},
         {
-          $and: [ // Implement filters here
-            { value: { $lte: maxValNum } },
+          $and: [
+            maxValue ? { value: { $lte: maxValue } } : {}, // Only implement if maxValue is defined
+            // Implement additional filters here
           ],
         },
       ],
     };
 
+    // Call search() controller with generated parameters
+    console.log(queryObject, sort, page, numPerPage);
     search(queryObject, sort, page, numPerPage).then((results) => {
       res.send({ results, numResults: results.length });
     }).catch((error) => {
