fb41134ab61c2fca85ae0bf9ddd6a1c22b213232
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2020-04-25 22:59:54 -0400

    Added requireSignin to signin route

--- src/routers/auth_router.js
+++ src/routers/auth_router.js
@@ -2,19 +2,18 @@ import express from 'express';
 
 import User from '../models/user_model';
 import { tokenForUser } from '../controllers/user_controller';
+import requireSignin from '../authentication/requireSignin';
 
 const router = express();
 
-// Send { id: #### } and server will send back authToken and user object
+
+// Send user object and server will send back authToken and user object
 router.route('/signin')
-  .post((req, res) => {
-    User.findById(req.body.id).then((user) => {
-      const json = user.toJSON();
-      delete json.password;
-      res.send({ token: tokenForUser(user), user });
-    }).catch((error) => {
-      res.status(500).send(error);
-    });
+  .post(requireSignin, (req, res) => {
+    // This information is loaded by passport
+    const json = req.user.toJSON();
+    delete json.password;
+    res.send({ token: tokenForUser(json), user: json });
   });
 
 export default router;
