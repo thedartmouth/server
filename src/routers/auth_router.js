613b3421bc90cdece067a85d2f2780ae1c8663a7
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2020-04-25 23:13:57 -0400

    Added working signup route

--- src/routers/auth_router.js
+++ src/routers/auth_router.js
@@ -1,11 +1,52 @@
 import express from 'express';
 
-import User from '../models/user_model';
 import { tokenForUser } from '../controllers/user_controller';
 import requireSignin from '../authentication/requireSignin';
+import User from '../models/user_model';
 
 const router = express();
 
+router.route('/signup')
+  .post((req, res) => {
+    const {
+      email, password, firstName, lastName,
+    } = req.body;
+
+    return User.findOne({ email }).then((user) => {
+      // Check if a user already has this email address
+      if (user) {
+        return res.status(409).send({ message: 'Email address already associated to a user' });
+      }
+
+      // Validate email and password
+      if (!email) {
+        // TODO: Validate email formatting
+        res.status(409).send({ message: 'Please enter an email address' });
+      } else if (!password) {
+        res.status(409).send({ message: 'Please enter a password' });
+      }
+
+      // Make a new user from passed data
+      const newUser = new User({
+        email: email.toLowerCase(),
+        password,
+        first_name: firstName,
+        last_name: lastName,
+      });
+
+      // Save the user then transmit to frontend
+      return newUser.save()
+        .then((savedUser) => {
+          const json = savedUser.toJSON();
+          delete json.password;
+          res.send({ token: tokenForUser(savedUser), user: json });
+        }).catch((error) => {
+          res.status(500).send(error);
+        });
+    }).catch((error) => {
+      res.status(500).send(error);
+    });
+  });
 
 // Send user object and server will send back authToken and user object
 router.route('/signin')
