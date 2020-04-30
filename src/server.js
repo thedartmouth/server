54840745b67f5ecd95cabf34d958bed9f1feed6d
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-30 16:26:42 -0400

    removed unused imports

--- src/server.js
+++ src/server.js
@@ -11,8 +11,6 @@ import searchRouter from './routers/search_router';
 
 import requireAuth from './authentication/requireAuth';
 
-import User from './models/user_model';
-
 // initialize
 const app = express();
 
