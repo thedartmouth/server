1e9ef00efc6c22af9ad976c1b1c4201a8d6ae2d0
ziruihao <ziray.hao.22@dartmouth.edu>
2020-05-01 21:55:32 -0400

    indexed controllers

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -1,9 +1,11 @@
 import jwt from 'jwt-simple';
 
-// import User from '../models/user_model';
-
 // eslint-disable-next-line import/prefer-default-export
-export function tokenForUser(user) {
+function tokenForUser(user) {
   const timestamp = new Date().getTime();
   return jwt.encode({ sub: user.id, iat: timestamp }, process.env.AUTH_SECRET);
 }
+
+const userController = { tokenForUser };
+
+export default userController;
