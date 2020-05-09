4829290dffd6756461c8807bdb45c03f46b27c76
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2020-05-09 17:40:55 -0400

    Added get all constants route

--- src/routers/constants_router.js
+++ src/routers/constants_router.js
@@ -4,6 +4,11 @@ import * as constants from '../constants';
 
 const router = express();
 
+// Return all constants to frontend as JSON
+router.get('/', (req, res) => {
+  return res.json({ filters: constants.FRONTEND_FILTERS });
+});
+
 // Return filters to frontend as JSON
 router.get('/filters', (req, res) => {
   return res.json(constants.FRONTEND_FILTERS);
