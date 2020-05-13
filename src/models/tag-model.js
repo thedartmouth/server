9a9af5fd17d2d9ac515f27902d1d878c01043d0d
ziruihao <ziray.hao.22@dartmouth.edu>
2020-05-13 18:33:04 -0400

    added tag  mongo model

---
+++ src/models/tag-model.js
@@ -0,0 +1,12 @@
+import mongoose, { Schema } from 'mongoose';
+
+const TagSchema = new Schema({
+  name: {type: String},
+  interested_users: [
+    {type: Schema.Types.ObjectId, ref: 'User' },
+  ],
+});
+
+const TagModel = mongoose.model('Tag', TagSchema);
+
+export default TagModel;
