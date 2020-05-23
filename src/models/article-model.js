e7420bf972e62fb88c194830979f14d8be8e42bf
ziruihao <ziray.hao.22@dartmouth.edu>
2020-05-23 01:09:50 -0400

    changed article model var names to camel case

--- src/models/article-model.js
+++ src/models/article-model.js
@@ -1,22 +1,22 @@
 import mongoose, { Schema } from 'mongoose';
 
 const ArticleSchema = new Schema({
-  ceo_id: { type: String },
-  web_url: {type: String},
-  category: [{type: Schema.Types.ObjectId, ref: 'Category' }],
-  tags: [{type: Schema.Types.ObjectId, ref: 'Tag' }],
+  CEOID: { type: String },
+  webURL: { type: String },
+  category: [{ type: Schema.Types.ObjectId, ref: 'Category' }],
+  tags: [{ type: Schema.Types.ObjectId, ref: 'Tag' }],
   headline: { type: String },
-  image_url: {type: String},
+  imageURL: { type: String },
   content: { type: String },
-  associated_polls: [
-    {type: Schema.Types.ObjectId, ref: 'Poll' }
+  associatedPolls: [
+    { type: Schema.Types.ObjectId, ref: 'Poll' },
   ],
-  created_at: {type: Date},
-  modified_at: {type: Date},
-  published_at: {type: Date},
-  views: {type: Number},
+  createdAt: { type: Date },
+  modifiedAt: { type: Date },
+  publishedAt: { type: Date },
+  views: { type: Number },
   authors: [
-    {type: Schema.Types.ObjectId, ref: 'Author' }
+    { type: Schema.Types.ObjectId, ref: 'Author' },
   ],
 });
 
