7284e161e511353de2ab7713adf0eec93a0f40fb
ziruihao <ziray.hao.22@dartmouth.edu>
2020-05-23 01:10:05 -0400

    refactored models

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,6 @@
-import UserModel from './user_model';
+import UserModel from './user-model';
 import ArticleModel from './article-model';
+import AuthorModel from './author-model';
 import PollModel from './poll-model';
 import TagModel from './tag-model';
 import CategoryModel from './category-model';
@@ -7,6 +8,7 @@ import CategoryModel from './category-model';
 export {
   UserModel as Users,
   ArticleModel as Articles,
+  AuthorModel as Authors,
   PollModel as Polls,
   TagModel as Tags,
   CategoryModel as Categories,
