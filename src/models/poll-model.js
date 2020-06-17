997202f49ea3988bc4e3a78c19532f3368938f1e
ziruihao <ziray.hao.22@dartmouth.edu>
2020-06-17 16:00:47 -0400

    removed unused fields

--- src/models/poll-model.js
+++ src/models/poll-model.js
@@ -2,15 +2,9 @@ import mongoose, { Schema } from 'mongoose';
 
 const PollSchema = new Schema({
   question: { type: String },
-  // answers: [
-  //   {
-  //     text: '',
-  //     count: 0,
-  //   },
-  // ],
-  answers: {type: Map}, 
-  associated_article: { type: Schema.Types.ObjectId, ref: 'Article'},
-  usersVoted: [{type: Schema.Types.ObjectID, ref: 'User', unique: true}], 
+  answers: { type: Map },
+  associatedArticle: { type: String, ref: 'Article' },
+  usersVoted: [{ type: Schema.Types.ObjectId, ref: 'User', unique: true }],
   created_at: { type: Date, default: Date.now() },
 });
 
