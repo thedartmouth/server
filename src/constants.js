21cf5c9c57bff2b05cd076323f321d86d1cdf2d2
Ziray Hao <zirayhao@Zirays-MacBook-Pro.local>
2020-12-01 21:41:18 -0500

    Remove PORT from constants

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,5 @@
 import packageInfo from '../package.json';
 
-export const PORT = process.env.PORT || 9090;
 export const MONGODB_URI = process.env.MONGODB_URI || packageInfo.localDatabaseURI;
 export const SELF_URL = process.env.NODE_ENV === 'development' ? `localhost:${9090}` : packageInfo.productionURL;
 export const APP_URL = process.env.NODE_ENV === 'development' ? `localhost:${8080}` : packageInfo.productionClientURL;
