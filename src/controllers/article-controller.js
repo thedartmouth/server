20744c052c521dedffdd73e25262daa9ba1ed2c6
ziruihao <ziray.hao.22@dartmouth.edu>
2020-12-05 15:53:57 -0500

    Migrated article controllers to PostgreSQL

--- src/controllers/article-controller.js
+++ src/controllers/article-controller.js
@@ -1,4 +1,3 @@
-import { Articles, Users } from '../models'
 import { query, getClient } from '../db'
 
 /**
@@ -6,54 +5,60 @@ import { query, getClient } from '../db'
  * @param {String} slug
  */
 async function fetchMetaArticle(slug) {
-	return query('SELECT * FROM metaArticles WHERE slug=$1', [slug])
+	return await query('SELECT * FROM metaArticles WHERE slug=$1', [slug])
 }
 
 /**
  * Creates a meta article references by slug.
+ * @param {String} slug
  */
-function createMetaArticle(slug) {
-	return query('INSERT INTO metaArticles (slug) VALUES ($1)', [slug])
+async function createMetaArticle(slug) {
+	await query('INSERT INTO metaArticles (slug) VALUES ($1)', [slug])
+	return 'meta article created'
 }
 
-// bookmarks an article if it's not been bookmarked, else unbookmarks it
+/**
+ * Toggles article bookmark for a user.
+ * @param {String} slug 
+ * @param {String} userId 
+ */
 async function bookmarkArticle(slug, userId) {
-	try {
-		const user = await Users.findById(userID)
-		const article = await Articles.findById(articleID)
-
-		const articleIndex = user.bookmarkedArticles.indexOf(articleID)
-		const userIndex = article.bookMarkedUsers.indexOf(userID)
-
-		if (articleIndex > -1 || userIndex > -1) {
-			// article is already bookmarked, remove bookmark
-			user.bookmarkedArticles.splice(articleIndex, 1)
-			article.bookMarkedUsers.splice(userIndex, 1)
-		} else {
-			user.bookmarkedArticles.push(articleID)
-			article.bookMarkedUsers.push(userID)
-		}
-		const savedUser = await user.save()
-		const savedArticle = await article.save()
-
-		return { user: savedUser, article: savedArticle }
-	} catch (err) {
-		return err.value === userID
-			? { message: 'Invalid userID', error: err }
-			: { message: 'Invalid articleID' }
+	const dbClient = await getClient()
+	const bookmarkExists = (
+		await dbClient.query(
+			'SELECT EXISTS(SELECT 1 FROM bookmarks WHERE articleSlug = $1 AND userId = $2)',
+			[slug, userId]
+		)
+	).rows[0].exists
+	if (bookmarkExists) {
+		await dbClient.query(
+			'DELETE FROM bookmarks WHERE articleSlug = $1 AND userId = $2',
+			[slug, userId]
+		)
+	} else {
+		await dbClient.query(
+			'INSERT INTO bookmarks (articleSlug, userId, timestamp) VALUES ($1, $2, $3)',
+			[slug, userId, new Date().toUTCString()]
+		)
 	}
+	dbClient.release()
+	return bookmarkExists ? 'bookmark deleted' : 'bookmark added'
 }
 
+/**
+ * Logs an artilce read for a user.
+ * @param {String} slug 
+ * @param {String} userId 
+ */
 async function readArticle(slug, userId) {
 	const dbClient = await getClient()
 
-	let metaArticle =
-		(
-			await dbClient.query('SELECT * FROM metaArticles WHERE slug = $1', [
-				slug,
-			])
-		).rows[0] || null
-	console.table(metaArticle)
+	let metaArticle = (
+		await dbClient.query(
+			'SELECT EXISTS(SELECT 1 FROM metaArticles WHERE slug = $1)',
+			[slug]
+		)
+	).rows[0].exists
 	if (!metaArticle) {
 		metaArticle = dbClient.query(
 			'INSERT INTO metaArticles (slug) VALUES ($1)',
@@ -70,29 +75,27 @@ async function readArticle(slug, userId) {
 		[slug]
 	)
 	dbClient.release()
+	return 'article read'
 }
 
-// works the same way as processReadArticle
-// finds an article and adds the user to the shared list
-async function shareArticle(article, user) {
-	const dbArticle = await Articles.findById(article.slug)
-	if (user) {
-		const sharedUser = dbArticle.sharedUsers.find((item) => {
-			return user._id.equals(item)
-		})
-		if (!sharedUser) {
-			dbArticle.sharedUsers.push(user._id)
-		}
-	}
-	await dbArticle.save()
-	return dbArticle
-}
+// async function shareArticle(article, user) {
+// 	const dbArticle = await Articles.findById(article.slug)
+// 	if (user) {
+// 		const sharedUser = dbArticle.sharedUsers.find((item) => {
+// 			return user._id.equals(item)
+// 		})
+// 		if (!sharedUser) {
+// 			dbArticle.sharedUsers.push(user._id)
+// 		}
+// 	}
+// 	await dbArticle.save()
+// 	return dbArticle
+// }
 
 export default {
 	createMetaArticle,
 	fetchMetaArticle,
-	createAndSaveArticle,
-	bookmarkArticle,
 	readArticle,
-	shareArticle,
+	bookmarkArticle,
+	// shareArticle,
 }
