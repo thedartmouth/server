be2f32775dc1249225460e01c1323b7c8d090715
ziruihao <ziray.hao.22@dartmouth.edu>
2020-12-05 12:54:05 -0500

    Refactor PostgreSQL connection to separate module

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
-import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import morgan from 'morgan';
 import dotenv from 'dotenv';
-import { Client as PostgreSQLClient } from 'pg';
+import express from 'express';
+import morgan from 'morgan';
+
+import {
+  articleRouter, authRouter, userRouter, feedRouter, pollRouter, authorRouter, tagRouter,
+} from './routers';
 
 dotenv.config();
 
@@ -19,10 +22,6 @@ app.use(morgan('dev'));
 // configure data type
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-import {
-  articleRouter, authRouter, userRouter, feedRouter, pollRouter, authorRouter, tagRouter,
-} from './routers';
 
 // declare routers
 app.use('/auth', authRouter);
@@ -42,15 +41,6 @@ app.get('/', (req, res) => {
 app.use((req, res) => {
   res.status(404).json({ message: 'The route you\'ve requested doesn\'t exist' });
 });
-
-const pgClient = new PostgreSQLClient({
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  port: process.env.PGPORT
-});
-pgClient.connect();
 
 // start the server
 app.listen(process.env.PORT);
