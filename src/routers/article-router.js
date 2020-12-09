aa91e8c17341c53bd1edd907dc44e0d35d18baa5
ziruihao <ziray.hao.22@dartmouth.edu>
2020-12-08 23:04:56 -0500

    Format

--- src/routers/article-router.js
+++ src/routers/article-router.js
@@ -12,45 +12,39 @@ articleRouter.route('/:slug').get(async (req, res) => {
 	}
 })
 
-articleRouter
-	.route('/read/:slug')
-	.post(
-		(req, res, next) =>
-			userController.validateUserExistence(req, res, next, req.body.userId),
-		async (req, res) => {
-			if (!req.params.slug) {
-				res.status(400).send('missing article slug')
-			} else {
-				try {
-					res.json(
-						await articleController.readArticle(
-							req.params.slug,
-							req.body.userId
-						)
+articleRouter.route('/read/:slug').post(
+	(req, res, next) =>
+		userController.validateUserExistence(req, res, next, req.body.userId),
+	async (req, res) => {
+		if (!req.params.slug) {
+			res.status(400).send('missing article slug')
+		} else {
+			try {
+				res.json(
+					await articleController.readArticle(
+						req.params.slug,
+						req.body.userId
 					)
-				} catch (err) {
-					res.status(500).send(err.message)
-				}
+				)
+			} catch (err) {
+				res.status(500).send(err.message)
 			}
 		}
-	)
+	}
+)
 
-articleRouter
-	.route('/share')
-	.post(async (req, res) => {
-		if (!req.body || !req.body.article) {
-			res.status(400).send('missing body or article')
-			return
-		}
-		try {
-			res.json(
-				await articleController.shareArticle(req.body.article, req.user)
-			)
-		} catch (error) {
-			console.log(error)
-			res.status(500).send('error processing shared article')
-		}
-	})
+articleRouter.route('/share').post(async (req, res) => {
+	if (!req.body || !req.body.article) {
+		res.status(400).send('missing body or article')
+		return
+	}
+	try {
+		res.json(await articleController.shareArticle(req.body.article, req.user))
+	} catch (error) {
+		console.log(error)
+		res.status(500).send('error processing shared article')
+	}
+})
 
 articleRouter.route('/').post(async (req, res) => {
 	if (!req.body.slug || typeof req.body.slug != 'string') {
