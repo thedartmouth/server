73a50cbb4129b9c0a1668a259f17cd2ac5a7af1d
ziruihao <ziray.hao.22@dartmouth.edu>
2020-12-08 23:04:28 -0500

    Create core user controllers

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,6 +1,13 @@
-import { Users } from '../models'
-import { query } from '../db'
+import { query, getClient } from '../db'
+import format from 'pg-format'
 
+/**
+ * Middleware to validate a user exists for routes that require querying on a specific user.
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ * @param {*} userId 
+ */
 const validateUserExistence = async (req, res, next, userId) => {
 	if (!userId) res.status(400).send('missing userId')
 	const user =
@@ -10,41 +17,59 @@ const validateUserExistence = async (req, res, next, userId) => {
 	next()
 }
 
-async function getUsers(filters) {
-	const results = await Users.find(filters).exec()
-	for (let result of results) {
-		result = redactUser(result.toObject())
+const createUser = async (user) => {
+	const user = Object.entries(user).map(entry => {
+		return `${format.ident(entry[0])} = ${format.literal(entry[1])}`
+	}).join(',')
+}
+
+/**
+ * Returns basic user data.
+ * @param {String} userId 
+ */
+const getBasicUserData = async (userId) => {
+	const dbClient = await getClient()
+	const user = (
+		await dbClient.query(
+			'SELECT firstName, lastName, email, reads FROM users WHERE userId = $1',
+			[userId]
+		)
+	).rows[0]
+	const res = {
+		name: {
+			first: user.firstName,
+			last: user.lastName,
+		},
+		email: user.email,
+		reads: user.reads,
 	}
-	return results
+	return res
 }
 
-function removeDocPassword(user) {
-	delete user._doc.password
-	return user
+const updateBasicUserData = async (userId, updates) => {
+	const updates = Object.entries(updates).map(entry => {
+		return `${format.ident(entry[0])} = ${format.literal(entry[1])}`
+	}).join(',')
+	await query(`UPDATE users SET ${updates} WHERE id = $1`, [userId])
 }
 
-function redactDocUser(user) {
-	delete user._doc.created_date
-	delete user._doc.password
-	return user
-}
-
-function removePassword(user) {
-	delete user.password
-	return user
-}
-
-function redactUser(user) {
-	delete user.created_date
-	delete user.password
-	return user
+/**
+ * Queries a user's bookmarked articles, sorted by most recent timestamp.
+ * @param {String} userId 
+ */
+const getBookmarkedArticles = async (userId) => {
+	const articles = (
+		await query(
+			'SELECT slug, timestamp FROM (SELECT articleSlug, timestamp FROM bookmarks WHERE userId = $1) AS userBookmarks LEFT JOIN (SELECT slug FROM metaArticles) AS matchingMetaArticles ON userBookmarks.articleSlug = matchingMetaArticles.slug ORDER BY timestamp DESC',
+			[userId]
+		)
+	).rows
+	return articles
 }
 
 export default {
 	validateUserExistence,
-	getUsers,
-	removePassword,
-	redactUser,
-	removeDocPassword,
-	redactDocUser,
+	getBasicUserData,
+	updateBasicUserData,
+	getBookmarkedArticles,
 }
