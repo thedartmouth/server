551fbda10145b83fe22802d9b85f64d4c02adea2
ziruihao <ziray.hao.22@dartmouth.edu>
2020-12-10 02:17:51 -0500

    Configure user endpoint routes

--- src/routers/user-router.js
+++ src/routers/user-router.js
@@ -1,69 +1,69 @@
-/* eslint-disable consistent-return */
 import express from 'express'
-import { Users } from '../models'
-import { requireAuth, requireAdmin } from '../authentication'
+import { UserValidationError } from '../modules/error'
+import { requireAuth, requireSelf } from '../authentication'
 import { userController } from '../controllers'
 
 const userRouter = express()
 
+userRouter.route('/').post(async (req, res) => {
+	try {
+		res.json({ userId: await userController.createUser(req.body) })
+	} catch (err) {
+		res.status(500).send(err.message)
+	}
+})
+
+userRouter.route('/auth').post(async (req, res) => {
+	if (!req.body?.email || !req.body?.password) {
+		res.status(400).send('bad {email} and {password} format')
+		return
+	}
+	try {
+		res.json(
+			await userController.generateToken(req.body.email, req.body.password)
+		)
+	} catch (err) {
+		if (err.message === 'invalid credentials')
+			res.status(401).send(err.message)
+		else res.status(500).send(err.message)
+	}
+})
+
 userRouter
-	.route('/')
-	.post(async (req, res) => {
+	.route('/:userId')
+	.get(async (req, res) => {
 		try {
-			res.json(await userController.createUser(req.body))
+			await userController.validateUserExistence(req.params.userId)(res)
+			requireSelf(req.body.userId, req)(res)
+			res.json(await userController.getBasicUserData(req.params.userId))
+		} catch (err) {
+			res.status(500).json(err)
+		}
+	})
+	.put(requireAuth({}), async (req, res) => {
+		try {
+			await userController.validateUserExistence(req.params.userId)(res)
+			requireSelf(req.body.userId, req)(res)
+			res.json(
+				await userController.updateBasicUserData(
+					req.params.userId,
+					req.body
+				)
+			)
 		} catch (err) {
 			res.status(500).send(err.message)
 		}
 	})
-
-userRouter
-	.route('/:userId')
-	.get(
-		(req, res, next) => userController.validateUserExistence(req, res, next, req.params.userId),
-		async (req, res) => {
-			try {
-				res.json(await userController.getBasicUserData(req.params.userId))
-			} catch (err) {
-				res.status(500).send(err.message)
-			}
-		}
-	)
-	.put(requireAuth({}), async (req, res) => {
-		if (!req.admin && req.user.id !== req.params.userId) {
-			res.setStatus(401)
-		} else {
-			try {
-				res.json(await userController.updateBasicUserData(req.params.userId, req.body))
-			} catch (err) {
-				res.status(500).send(err.message)
-			}
-		}
-	})
 	.delete(requireAuth({}), async (req, res) => {
-		if (!req.admin && req.user.id !== req.params.userId) {
-			res.setStatus(401)
-		} else {
-			try {
-				res.json(await userController.deleteUser(req.params.userId))
-			} catch (err) {
+		try {
+			await userController.validateUserExistence(req.params.userId)(res)
+			requireSelf(req.body.userId, req)(res)
+			await userController.deleteUser(req.params.userId)
+			res.sendStatus(200)
+		} catch (err) {
+			if (!(err instanceof UserValidationError))
 				res.status(500).send(err.message)
-			}
 		}
 	})
-
-userRouter
-	.route('/bookmarks/:userId')
-	.get(
-		(req, res, next) => userController.validateUserExistence(req, res, next, req.params.userId),
-		async (req, res) => {
-			try {
-				res.json(
-					await userController.getBookmarkedArticles(req.params.userId)
-				)
-			} catch (err) {
-				res.status(500).send(err.message)
-			}
-		}
-	)
 
 export default userRouter
