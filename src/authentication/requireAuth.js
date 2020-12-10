78f4536f7a0030bdd36d18b19a48e14847b47625
ziruihao <ziray.hao.22@dartmouth.edu>
2020-12-10 02:17:01 -0500

    Migrated passport.js authentication with PostgreSQL database

--- src/authentication/requireAuth.js
+++ src/authentication/requireAuth.js
@@ -2,9 +2,8 @@
 import passport from 'passport'
 import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt'
 import jwt from 'jwt-simple'
+import { query } from '../db'
 import dotenv from 'dotenv'
-
-import User from '../models/user-model'
 
 dotenv.config({ silent: true })
 
@@ -15,57 +14,51 @@ const jwtOptions = {
 
 export function tokenForUser(user) {
 	const timestamp = new Date().getTime()
-	return jwt.encode({ sub: user.id, iat: timestamp }, process.env.AUTH_SECRET)
+	return jwt.encode({ userId: user.id, timestamp: timestamp }, process.env.AUTH_SECRET)
 }
 
-const jwtAuthLogin = new JwtStrategy(jwtOptions, (payload, done) => {
-	// See if the token matches any user document in the DB
-	// Done function in the form -> "done(resulting error, resulting user)"
-	User.findById(payload.sub, (err, user) => {
-		// This logic can be modified to check for user attributes
-		if (err) {
-			return done(err, false) // Error return
-		} else if (user) {
-			return done(null, user) // Valid user return
-		} else {
-			return done(null, false) // Catch no valid user return
-		}
-	})
+const jwtAuthLogin = new JwtStrategy(jwtOptions, async (payload, done) => {
+	const user = (await query('SELECT id FROM users WHERE id = $1', [payload.userId])) || null
+	if (user) {
+		return done(null, user)
+	} else {
+		return done(null, false)
+	}
 })
 
 passport.use('jwt-auth', jwtAuthLogin)
 
 // Create function to transmit result of authenticate() call to user or next middleware
-const requireAuth = (options) => function (req, res, next) {
-	if (req.headers['api_key'] === process.env.API_KEY) {
-		req.admin = true
-		next()
-	}
-	else if (!options.admin) {
-		passport.authenticate(
-			'jwt-auth',
-			{ session: false },
-			function (err, user, info) {
-				if (err) {
-					return next(err)
+const requireAuth = (options) =>
+	function (req, res, next) {
+		if (req.headers['api_key'] === process.env.API_KEY) {
+			req.admin = true
+			next()
+		} else if (!options.admin) {
+			passport.authenticate(
+				'jwt-auth',
+				{ session: false },
+				function (err, user, info) {
+					if (err) {
+						return next(err)
+					}
+
+					if (!user) {
+						return res.status(401).json({
+							message: info
+								? info.message
+								: 'Authentication Error: Not authorized to perform this request',
+						})
+					}
+
+					req.user = user
+
+					return next()
 				}
-	
-				if (!user) {
-					return res.status(401).json({
-						message: info
-							? info.message
-							: 'Authentication Error: Not authorized to perform this request',
-					})
-				}
-	
-				req.user = user
-	
-				return next()
-			}
-		)(req, res, next)
-	} else {
-		return res.setStatus(401)
+			)(req, res, next)
+		} else {
+			return res.sendStatus(401)
+		}
 	}
-}
 
 export default requireAuth
