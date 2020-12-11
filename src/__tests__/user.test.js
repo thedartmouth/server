0c3ef3cfa25d3710f805fc602824be81e98e7a59
ziruihao <ziray.hao.22@dartmouth.edu>
2020-12-10 23:32:30 -0500

    Add test case for updating user

--- src/__tests__/user.test.js
+++ src/__tests__/user.test.js
@@ -23,7 +23,6 @@ export default () =>
 					.set('Accept', 'application/json')
 				expect(createRes.statusCode).toBe(200)
 				expect(uuid.validate(createRes.body.userId)).toBe(true)
-				console.info(`Newly created userId ${createRes.body.userId}`)
 				user.id = createRes.body.userId
 
 				const getRes = await request(app)
@@ -38,7 +37,6 @@ export default () =>
 					email: user.email,
 					reads: 0,
 				})
-				console.table(getRes.body)
 			})
 		})
 
@@ -85,6 +83,51 @@ export default () =>
 					.set('Accept', 'application/json')
 				expect(getRes.statusCode).toBe(401)
 				expect(getRes.text).toBe('invalid credentials')
+			})
+		})
+
+		describe('successfully', () => {
+			test('updates a user', async () => {
+				expect.assertions(7)
+
+				const updatedUser = {
+					id: 'cannot_set',
+					firstName: 'Test_updated',
+					lastName: 'User_updated',
+					email: `${uuid.v4()}@email.com`,
+					password: 'good_password_updated',
+				}
+
+				const updateRes = await request(app)
+					.put(path)
+					.send(updatedUser)
+					.set('Accept', 'application/json')
+				expect(updateRes.statusCode).toBe(200)
+				expect(uuid.validate(updateRes.body.userId)).toBe(true)
+				expect(updateRes.body.userId === user.id).toBe(true)
+				
+				const authRes = await request(app)
+					.post(`${path}/auth`)
+					.send({
+						email: updatedUser.email,
+						password: updatedUser.password,
+					})
+					.set('Accept', 'application/json')
+				expect(authRes.statusCode).toBe(200)
+				expect(authRes.body?.userId).toBe(user.id)
+
+				const getRes = await request(app)
+					.get(`${path}/${updateRes.body.userId}`)
+					.set('API_KEY', process.env.API_KEY)
+				expect(getRes.statusCode).toBe(200)
+				expect(getRes.body).toEqual({
+					name: {
+						first: updatedUser.firstName,
+						last: updatedUser.lastName,
+					},
+					email: updatedUser.email,
+					reads: 0,
+				})
 			})
 		})
 
