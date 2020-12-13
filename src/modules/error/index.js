42d9d56b093476c738aeb3a0fe145c419c2b9076
ziruihao <ziray.hao.22@dartmouth.edu>
2020-12-12 19:10:11 -0500

    Console error logging errors that go through middleware

--- src/modules/error/index.js
+++ src/modules/error/index.js
@@ -13,6 +13,7 @@ export class UserValidationError extends Error {
 }
 
 export const errorHandlerMiddleware = (err, req, res, next) => {
+	console.error(err)
 	if (res.headersSent) {
 		return next(err)
 	}
