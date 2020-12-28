27b47ad95ea1ca6f3bb2c072aef51a229ea645d1
ziruihao <ziray.hao.22@dartmouth.edu>
2020-12-28 09:44:55 -0500

    Fixup populate

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,6 @@
 import userController from './user-controller'
 import pollController from './poll-controller'
 import articleController from './article-controller'
-import populateAll from './populate'
 import feedController from './feed-controller'
 import authorController from './author-controller'
 import tagController from './tag-controller'
@@ -9,7 +8,6 @@ import tagController from './tag-controller'
 export {
 	userController,
 	articleController,
-	populateAll,
 	pollController,
 	feedController,
 	authorController,
