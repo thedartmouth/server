0fb70889a4599e4d1e58a47bd661c89ff5be29d2
ziruihao <ziray.hao.22@dartmouth.edu>
2021-05-17 00:11:32 -0600

    Build notification firing modules

--- src/modules/notifications/apple.js
+++ src/modules/notifications/apple.js
@@ -1,8 +1,8 @@
 /**
- * 
+ *
  * @param {*} notification Requested notification to fire.
  * @return {'SUCCESS' | string} Return 'SUCCESS' or the reason for failure.
  */
 export async function fire(notification) {
- // to-do @alex build function to fire off notification to Apple's servers
+	// to-do @alex build function to fire off notification to Apple's servers
 }
