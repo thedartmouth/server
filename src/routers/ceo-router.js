d2fef974ce0ecb7f5aec00393dbd00934a89b7bb
ziruihao <ziray.hao.22@dartmouth.edu>
2021-06-01 10:17:35 -0600

    Add status to manage CEO Connector API

--- src/routers/ceo-router.js
+++ src/routers/ceo-router.js
@@ -6,7 +6,8 @@ const ceoRouter = express()
 
 ceoRouter.route('/connector').post(
 	asyncHandler(async (req, res) => {
-		ceoController.handleData(req.body)
+		if (process.env.CEO_CONNECTOR_ENABLED === 'true')
+			ceoController.handleData(req.body)
 		res.sendStatus(200)
 	})
 )
