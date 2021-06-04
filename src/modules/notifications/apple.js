7efd5be0a6f76be5667fad53d303fe6a3084b972
ziruihao <ziray.hao.22@dartmouth.edu>
2021-06-04 14:47:03 -0600

    Remove old nofiications modules

--- src/modules/notifications/apple.js
+++
@@ -1,8 +0,0 @@
-/**
- *
- * @param {*} notification Requested notification to fire.
- * @return {'SUCCESS' | string} Return 'SUCCESS' or the reason for failure.
- */
-export async function fire(notification) {
-	// to-do @alex build function to fire off notification to Apple's servers
-}
--- src/modules/notifications/index.js
+++ src/modules/notifications/index.js
@@ -1,13 +1,6 @@
-import { fire as fireAppleNotification } from './apple'
-import { fire as fireFirebaseNotification } from './firebase'
 import { fire as fireExpoNotification } from './expo'
 export * from './notification'
 
 export async function fireNotification(notificationId) {
 	return fireExpoNotification(notificationId)
-	// return {
-	// 	// apple: await fireAppleNotification(notification),
-	// 	// firebase: await fireFirebaseNotification(notification),
-	// 	expo: await fireExpoNotification(notification)
-	// }
 }
